e801cd510f91b579840ba75293a606d01cd27505
Pratyush singh <ps432241@gmail.com>
2025-04-01 16:03:05 +0530

    add forntend url

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,13 +3,14 @@ import { useAuthStore } from "../store/useAuthStore";
 import AuthImagePattern from "../components/AuthImagePattern";
 import { Link } from "react-router-dom";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
-
+const BASE_URL =import.meta.env.VITE_BASE_URL;
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+  console.log(BASE_URL)
   const { login, isLoggingIn } = useAuthStore();
 
   const handleSubmit = async (e) => {
