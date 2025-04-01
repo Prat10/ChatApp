c6138552995dadd71c8278fa96e9c34aedb3f61a
Pratyush singh <ps432241@gmail.com>
2025-04-01 16:28:36 +0530

    chamges in backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const BASE_URL =import.meta.env.VITE_BASE_URL;
+// const BASE_URL =import.meta.env.VITE_BASE_URL;
 
 export const axiosInstance = axios.create({
-  // baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : `${import.meta.env.VITE_BASE_URL}/api`,
-  baseURL:BASE_URL,
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : `${import.meta.env.VITE_BASE_URL}/api`,
+  // baseURL:BASE_URL,
   withCredentials: true,
 });
